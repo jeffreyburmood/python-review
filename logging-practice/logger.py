90146b4b44d80aa8c1bce0780ab55b245d22c505
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2023-10-21 13:30:34 -0600

    Update logger.py

--- logging-practice/logger.py
+++ logging-practice/logger.py
@@ -4,14 +4,33 @@
 
 import logging
 
+# setup logger and provide name
+logger = logging.getLogger("testLogger")
+
+# setup logging level
+logger.setLevel(logging.DEBUG)
+
 # setup the handlers
 console_handler = logging.StreamHandler()
 file_handler = logging.FileHandler("file.log")
 
-# configure the logger
-logging.basicConfig(level=logging.DEBUG, handlers=[console_handler, file_handler])
+# create the formatter (same for both handlers in this example
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s:%(lineno)d %(message)s")
 
-logger = logging.getLogger("testLogger")
+# add formatter to handlers
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+# add handler to logger
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+# configure the logger
+# logging.basicConfig(
+#    level=logging.DEBUG,
+#    format="%(asctime)s %(levelname)s %(name)s:%(lineno)d %(message)s",
+#    handlers=[console_handler, file_handler]
+# )
 
 logger.debug("This is a debug message")
 logger.info("This is an info message")
