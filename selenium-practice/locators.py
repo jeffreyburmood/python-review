8ae543a8639214783efcf4e84b851c11e7d80133
Jeffrey Burmood <Jeffrey-swdev@proton.me>
2023-10-25 06:00:22 -0600

    Update locators.py
    Update methods for selenium 4.x

--- selenium-practice/locators.py
+++ selenium-practice/locators.py
@@ -16,25 +16,25 @@ def main():
     driver.get('https://rahulshettyacademy.com/locatorspractice/')
 
     # first try to login in using bad credentials
-    driver.find_element_by_id('inputUsername').send_keys('tester')
-    driver.find_element_by_name('inputPassword').send_keys('random passwd')
-    driver.find_element_by_class_name('signInBtn').click()
+    driver.find_element(By.ID,'inputUsername').send_keys('tester')
+    driver.find_element(By.NAME,'inputPassword').send_keys('random passwd')
+    driver.find_element(By.CLASS_NAME, 'signInBtn').click()
     # verify incorrect name or password message
-    print(driver.find_element_by_class_name('error').text)
+    print(driver.find_element(By.CLASS_NAME,'error').text)
 
     # click on Forgot Password and create a user login
-    driver.find_element_by_link_text('Forgot your password?').click()
+    driver.find_element(By.LINK_TEXT,'Forgot your password?').click()
 
     time.sleep(3) # pause so we can see the website
 
-    driver.find_element_by_css_selector('input[placeholder="Name"]').send_keys('john')
-    driver.find_element_by_css_selector('input[placeholder="Email"]').send_keys('john@email.com')
-    driver.find_element_by_css_selector('input[placeholder="Phone Number"]').send_keys('4804409982')
+    driver.find_element(By.CSS_SELECTOR,'input[placeholder="Name"]').send_keys('john')
+    driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Email"]').send_keys('john@email.com')
+    driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Phone Number"]').send_keys('4804409982')
 
-    driver.find_element_by_css_selector('.reset-pwd-btn').click()
-    print(driver.find_element_by_css_selector('.infoMsg').text)
+    driver.find_element(By.CSS_SELECTOR,'.reset-pwd-btn').click()
+    print(driver.find_element(By.CSS_SELECTOR,'.infoMsg').text)
 
-    driver.find_element_by_css_selector('.go-to-login-btn').click()
+    driver.find_element(By.CSS_SELECTOR,'.go-to-login-btn').click()
 
     time.sleep(3)
 
