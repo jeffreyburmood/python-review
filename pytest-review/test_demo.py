646b788c875692e20a23cffad2be5b13405aa620
Jeffrey Burmood <Jeffrey-swdev@proton.me>
2023-10-27 09:56:23 -0600

    Update test_demo.py
    pytest upractice pdates

--- pytest-review/test_demo.py
+++ pytest-review/test_demo.py
@@ -1,5 +1,16 @@
-""" Some pytest practice code """
+""" Some pytest practice code
+
+    use -k for regex type test selection based on test name
+
+    use @pytest.mark for grouping based on smoke, regression, etc """
+
+import pytest
 
 
+@pytest.mark.smoke
 def test_quicktest():
     print('Hello from pytest')
+
+
+def test_anotherquicktest():
+    print('Hello again from pytest')
